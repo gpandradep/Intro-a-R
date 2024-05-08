f642125bea88dd7707a15d38f2397b9f359ec29b
klvelasquezc <47877185+klvelasquezc@users.noreply.github.com>
2024-05-07 22:51:17 -0600

    Intro R Update

---
+++ posts/Operadores objetos y funciones/ObjOpeFun.R
@@ -0,0 +1,34 @@
+#
+# Objetos, Operadores y funciones
+# Karen Velásquez-C
+# 2024
+#
+######################################
+
+## Ejecutar comandos
+
+# Vamos a comenzar con algo sencillo:
+
+2+2
+
+# También podemos usar paréntesis para agrupar operaciones
+
+(50+9)/(2*3)
+
+# En R a veces queremos escribir notas o "comentar". En esos casos usamos el numeral o gato (para los mexicanos) `#`
+
+#Esto no lo ejecuta R porque tiene un # al inicio
+#objeto <- 23, esto tampoco funcionará
+2 # este es un número y su se ejecuta porque el númeral esta despues del objeto, o función
+
+## Crear objetos
+
+# En R definimos un objeto mediante `<-` (Alt + "-"), por ejemplo:
+
+numero <- 2
+numerote <- 56
+
+# si yo ejecuto la linea de numerote, que número saldrá?
+
+numerote
+
