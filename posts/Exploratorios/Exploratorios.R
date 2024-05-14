c32797fc27dd5890b271c52e45809032a650adec
klvelasquezc <47877185+klvelasquezc@users.noreply.github.com>
2024-05-13 23:03:57 -0600

    update 2024

---
+++ posts/Exploratorios/Exploratorios.R
@@ -0,0 +1,26 @@
+######################################
+#
+#  Análisis exploratorios
+#  Karen Velásquez- C
+#  2024
+#
+######################################
+
+
+# Instalación paquetería "lterdatasampler" 
+
+#install.packages("lterdatasampler")
+library(lterdatasampler)  # Cargar libreria
+
+# Cargar datos de cangrejos
+
+crab <- pie_crab
+View(crab)
+
+summary(crab)
+
+## Exploración gráfica
+
+
+
+
